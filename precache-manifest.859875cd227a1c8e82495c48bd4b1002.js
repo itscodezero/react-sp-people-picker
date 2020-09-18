c2f5e594892139a879e4b1909adcba9b06f62c07
sanjivanidelekar10@gmail.com <sanjivanidelekar@gmail.com>
2020-09-18 19:44:20 +0530

    Updates

---
+++ precache-manifest.859875cd227a1c8e82495c48bd4b1002.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9f7482eaf1c87c2965f3aa9345e0d0e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1d38cda1ac05fdc7a03a",
+    "url": "./static/css/main.977b6351.chunk.css"
+  },
+  {
+    "revision": "88389cb9598c1257b4d0",
+    "url": "./static/js/2.2f2d74f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2f2d74f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d38cda1ac05fdc7a03a",
+    "url": "./static/js/main.5693ff87.chunk.js"
+  },
+  {
+    "revision": "eb9753ec3918d010a617",
+    "url": "./static/js/runtime-main.27caca38.js"
+  }
+]);
